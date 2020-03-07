9fcace1c8cefb2c11625ecb0c4796c62b180ef58
Caíque Araújo Spósito <kiqaps@gmail.com>
2020-03-07 19:24:21 -0300

    Updates

---
+++ precache-manifest.30609db50896c7c3b331d42865ad3d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c188c73e991015641c1e06130468c53",
+    "url": "/kiqaps-cv/index.html"
+  },
+  {
+    "revision": "409de9db0aea090c7e64",
+    "url": "/kiqaps-cv/static/css/main.ae589795.chunk.css"
+  },
+  {
+    "revision": "d154383f79913482625d",
+    "url": "/kiqaps-cv/static/js/2.ed10cb1c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kiqaps-cv/static/js/2.ed10cb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409de9db0aea090c7e64",
+    "url": "/kiqaps-cv/static/js/main.062579ea.chunk.js"
+  },
+  {
+    "revision": "1fe6b0c217705d1de972",
+    "url": "/kiqaps-cv/static/js/runtime-main.a634b4e3.js"
+  }
+]);
